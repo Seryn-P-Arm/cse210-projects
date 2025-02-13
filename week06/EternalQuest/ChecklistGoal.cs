4eeb936d022e513847c693c568912690c46c7b23
Seryn-P-Arm <serynarmstrong@gmail.com>
2025-02-13 20:52:36 +0200

    update

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -7,8 +7,7 @@ public class ChecklistGoal : Goal
     private int _completed;
     private int _bonus;
 
-    public ChecklistGoal(string name, string description, int points, int target, int bonus)
-        : base(name, description, points)
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
     {
         _target = target;
         _completed = 0;
